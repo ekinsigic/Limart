9fff80cb7a95eb1f456d71822e1dc205f87e9574
ekinsigic <ekin@Ozguns-MacBook-Pro.local>
2014-08-12 22:15:37 +0300

    Product detail sayfas

---
+++ Scripts/ProductDetail.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function () {
+    triggerGallerySlider();
+    if ($('.topFullWidth h5').css('display') == 'none') {
+        carryHeading();
+    };
+});
+$(window).resize(function(){
+    if ($('.topFullWidth h5').css('display') == 'none') {
+        carryHeading();
+    };
+});
+function triggerGallerySlider() {
+    $('#gallerySlider').royalSlider({
+        addActiveClass: true,
+        arrowsNav: !isMobile,
+        arrowsNavAutoHide: false,
+        controlsInside: false,
+        controlNavigation: 'none',
+        autoScaleSlider: true,
+        autoScaleSliderWidth: 960,
+        autoScaleSliderHeight: 340,
+        loop: true,
+        fadeinLoadedSlide: false,
+        globalCaption: true,
+        keyboardNavEnabled: true,
+        globalCaptionInside: false,
+        dragUsingMouse: true,
+        numImagesToPreload: 8,
+        usePreloader: true,
+        slidesSpacing:0,
+        visibleNearby: {
+            enabled: true,
+            centerArea: isMobile ? 0.60 : 0.45,
+            center: true,
+            breakpoint: 0,
+            breakpointCenterArea: 0,
+            navigateByCenterClick: false
+        }
+    });
+}
+
+function carryHeading() {
+    var itemsToCarry = $('.artworkHeading')
+    $('.topFullWidth').append(itemsToCarry);
+}

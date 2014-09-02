65562f82b88f85168856bd703948cc7e8a5abc00
aslhnates <aslihanates91@gmail.com>
2014-09-02 14:25:17 +0100

    SON HAL

---
+++ Scripts/63/AccountDetails.js
@@ -0,0 +1,29 @@
+﻿$(window).load(function () {
+
+});
+
+$(window).resize(function () {
+
+});
+
+$(document).ready(function () {
+   
+});
+
+
+
+
+
+var isAccountMenuActive = false;
+
+function openOrCloseAccountMenu() {
+
+    if (!isAccountMenuActive) {
+        isAccountMenuActive = true;
+        $('#accountDetails .divDetail, footer').addClass('activeMenu');
+    }
+    else {
+        $('#accountDetails .divDetail, footer').removeClass('activeMenu');
+        isAccountMenuActive = false;
+    }
+}

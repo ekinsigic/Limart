8cf78f67e09fdfacb30ba07c68a34042f30f7444
ekinsigic <ekin@Ozguns-MacBook-Pro.local>
2014-09-09 12:46:37 +0300

    geniş commit

--- Scripts/SSTLogin.js
+++ Scripts/SSTLogin.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    getSocialIcons();
     loginPageAjaxCall();
 })
 
@@ -14,25 +15,26 @@ function loginPageAjaxCall() {
             $(panelNow).removeClass('activeInnerOpener');
             $('main').css('height',$('main').height());
             $('#userOpenerInnerDivLogin').css({'opacity':'0',
-                                                    '-webkit-transform':'scale(0.95)',
-                                                    'transition':'opacity 0.3s, transform 0.3s'});
+                '-webkit-transform':'scale(0.95)',
+                'transition':'opacity 0.3s, transform 0.3s'});
             setTimeout(function(){
             $('html, body').scrollTop(0);
                 $.get( 'signup.html', function(data){
                     $('.loginPage').append($(data).find('#userOpenerInnerDivSignUp'));
                     $('#userOpenerInnerDivSignUp').css({'display':'block',
-                                                            'opacity':'0',
-                                                            '-webkit-transform':'scale(0.95)',
-                                                            'transition':'opacity 0.3s, transform 0.3s'});
+                        'opacity':'0',
+                        '-webkit-transform':'scale(0.95)',
+                        'transition':'opacity 0.3s, transform 0.3s'});
+                signUpPageAjaxCall();
+                miscImgLoader();
+                    setTimeout(function(){
+                        $('#userOpenerInnerDivSignUp').css({'display':'block',
+                            'opacity':'1',
+                            '-webkit-transform':'scale(1)',
+                            'transition':'opacity 0.3s, transform 0.3s'});
+                        $('#userOpenerInnerDivLogin').remove();
+                    },500)
                 })
-                setTimeout(function(){
-                    $('#userOpenerInnerDivSignUp').css({'display':'block',
-                                                            'opacity':'1',
-                                                            '-webkit-transform':'scale(1)',
-                                                            'transition':'opacity 0.3s, transform 0.3s'});
-                    $('#userOpenerInnerDivLogin').remove();
-                    signUpPageAjaxCall();
-                },200)
             },500);
         });
 
@@ -46,25 +48,26 @@ function loginPageAjaxCall() {
             $(panelNow).removeClass('activeInnerOpener');
             $('main').css('height',$('main').height());
             $('#userOpenerInnerDivLogin').css({'opacity':'0',
-                                                    '-webkit-transform':'scale(0.95)',
-                                                    'transition':'opacity 0.3s, transform 0.3s'});
+                '-webkit-transform':'scale(0.95)',
+                'transition':'opacity 0.3s, transform 0.3s'});
             setTimeout(function(){
             $('html, body').scrollTop(0);
                 $.get( 'ForgotPassword.html', function(data){
                     $('.loginPage').append($(data).find('#userOpenerInnerDivForgotPassword'));
                     $('#userOpenerInnerDivForgotPassword').css({'display':'block',
-                                                            'opacity':'0',
-                                                            '-webkit-transform':'scale(0.95)',
-                                                            'transition':'opacity 0.3s, transform 0.3s'});
+                        'opacity':'0',
+                        '-webkit-transform':'scale(0.95)',
+                        'transition':'opacity 0.3s, transform 0.3s'});
+                forgotPasswordPageAjaxCall();
+                miscImgLoader();
+                    setTimeout(function(){
+                        $('#userOpenerInnerDivForgotPassword').css({'display':'block',
+                            'opacity':'1',
+                            '-webkit-transform':'scale(1)',
+                            'transition':'opacity 0.3s, transform 0.3s'});
+                        $('#userOpenerInnerDivLogin').remove();
+                    },500)
                 })
-                setTimeout(function(){
-                    $('#userOpenerInnerDivForgotPassword').css({'display':'block',
-                                                            'opacity':'1',
-                                                            '-webkit-transform':'scale(1)',
-                                                            'transition':'opacity 0.3s, transform 0.3s'});
-                    $('#userOpenerInnerDivLogin').remove();
-                    forgotPasswordPageAjaxCall();
-                },200)
             },500);
         });
 }
@@ -81,25 +84,27 @@ function signUpPageAjaxCall() {
             $(panelNow).removeClass('activeInnerOpener');
             $('main').css('height',$('main').height());
             $('#userOpenerInnerDivSignUp').css({'opacity':'0',
-                                                    '-webkit-transform':'scale(0.95)',
-                                                    'transition':'opacity 0.3s, transform 0.3s'});
+                '-webkit-transform':'scale(0.95)',
+                'transition':'opacity 0.3s, transform 0.3s'});
             setTimeout(function(){
             $('html, body').scrollTop(0);
                 $.get( 'login.html', function(data){
                     $('.loginPage').append($(data).find('#userOpenerInnerDivLogin'));
                     $('#userOpenerInnerDivLogin').css({'display':'block',
-                                                            'opacity':'0',
-                                                            '-webkit-transform':'scale(0.95)',
-                                                            'transition':'opacity 0.3s, transform 0.3s'});
+                        'opacity':'0',
+                        '-webkit-transform':'scale(0.95)',
+                        'transition':'opacity 0.3s, transform 0.3s'});
+                loginPageAjaxCall();
+                getSocialIcons();
+                miscImgLoader();
+                    setTimeout(function(){
+                        $('#userOpenerInnerDivLogin').css({'display':'block',
+                            'opacity':'1',
+                            '-webkit-transform':'scale(1)',
+                            'transition':'opacity 0.3s, transform 0.3s'});
+                        $('#userOpenerInnerDivSignUp').remove();
+                    },500)
                 })
-                setTimeout(function(){
-                    $('#userOpenerInnerDivLogin').css({'display':'block',
-                                                            'opacity':'1',
-                                                            '-webkit-transform':'scale(1)',
-                                                            'transition':'opacity 0.3s, transform 0.3s'});
-                    $('#userOpenerInnerDivSignUp').remove();
-                    loginPageAjaxCall();
-                },200)
             },500);
         });
 }
@@ -115,25 +120,36 @@ function forgotPasswordPageAjaxCall() {
             $(panelNow).removeClass('activeInnerOpener');
             $('main').css('height',$('main').height());
             $('#userOpenerInnerDivForgotPassword').css({'opacity':'0',
-                                                    '-webkit-transform':'scale(0.95)',
-                                                    'transition':'opacity 0.3s, transform 0.3s'});
+                '-webkit-transform':'scale(0.95)',
+                'transition':'opacity 0.3s, transform 0.3s'});
             setTimeout(function(){
             $('html, body').scrollTop(0);
                 $.get( 'login.html', function(data){
                     $('.loginPage').append($(data).find('#userOpenerInnerDivLogin'));
                     $('#userOpenerInnerDivLogin').css({'display':'block',
-                                                            'opacity':'0',
-                                                            '-webkit-transform':'scale(0.95)',
-                                                            'transition':'opacity 0.3s, transform 0.3s'});
-                })
-                setTimeout(function(){
-                    $('#userOpenerInnerDivLogin').css({'display':'block',
-                                                            'opacity':'1',
-                                                            '-webkit-transform':'scale(1)',
-                                                            'transition':'opacity 0.3s, transform 0.3s'});
-                    $('#userOpenerInnerDivForgotPassword').remove();
+                        'opacity':'0',
+                        '-webkit-transform':'scale(0.95)',
+                        'transition':'opacity 0.3s, transform 0.3s'});
                     loginPageAjaxCall();
-                },200)
+                    getSocialIcons();
+                    miscImgLoader();
+                    setTimeout(function(){
+                        $('#userOpenerInnerDivLogin').css({'display':'block',
+                            'opacity':'1',
+                            '-webkit-transform':'scale(1)',
+                            'transition':'opacity 0.3s, transform 0.3s'});
+                        $('#userOpenerInnerDivForgotPassword').remove();
+                    },500)
+                })
             },500);
         });
+}
+
+
+function getSocialIcons() {
+    $('.divSocialIcons').load('SocialLogin.html #socialIconsHandler', function(){
+        setTimeout(function(){
+            miscImgLoader();
+        },100);
+    });
 }
--- Scripts/SSTPayment.js
+++ Scripts/SSTPayment.js
@@ -1,221 +1,127 @@
 $(document).ready(function(){
-	openOrCloseNewAddress('input[name=group1]:radio', '#payment .deliveryAddressWrapper', 'newAddress,savedAddress');
-	openOrCloseNewAddress('input[name=group2]:radio', '#payment .divBillingAddress', 'newAddress,savedAddress');
-	openClosedSections('#cbAddressSelection2','#payment .billingAddressWrapper',20);
-	openClosedSections('#cbDifferentPerson','#payment .divAnotherRecipientContent',0);
-	openClosedSections('#cbGiftNote','#payment .divGiftNoteTextareaWrapper',0);
+	deliveryAddressRadioChange();
+	invoiceAddressRadioChange();
+	openCloseElements('#cbAddressSelection2','.billingAddressWrapper');
+	openCloseElements('#cbDifferentPerson','.divAnotherRecipientContent');
+	openCloseElements('#cbGiftNote','.divGiftNoteTextareaWrapper');
 	radioActions();
+
+	$('input[name="group1"]').each(function(){
+		$(this).change(function(){
+			deliveryAddressRadioChange();
+		})
+	});
+	$('input[name="group2"]').each(function(){
+		$(this).change(function(){
+			invoiceAddressRadioChange();
+		})
+	});
+
+		$('#cbAddressSelection2').change(function(){
+			openCloseElements('#cbAddressSelection2','.billingAddressWrapper');
+		});
+		$('#cbDifferentPerson').change(function(){
+			openCloseElements('#cbDifferentPerson','.divAnotherRecipientContent');
+		});
+		$('#cbGiftNote').change(function(){
+			openCloseElements('#cbGiftNote','.divGiftNoteTextareaWrapper');
+		});
 });
 
+var selectedOption = 'saved'
+var nonSelectedOption = 'new'
 
-function openOrCloseNewAddress(triggerInput,affectedElement,classNamesToggle) {
-	$(affectedElement).wrapInner('<div class="innerWrapper">');
-	var classNames = (classNamesToggle).split(',');
-	$(affectedElement).addClass(classNames[1]);
-	$(affectedElement).removeClass(classNames[0]);
-	var currentHeight = $(affectedElement).find('.innerWrapper').outerHeight();
-	$(affectedElement).find('.innerWrapper > *').unwrap();
-	$(affectedElement).height(currentHeight);
-	$(affectedElement).css({
-		'opacity':'1',
-		'overflow':'hidden',
-		'-webkit-transform':'scale(1)s',
-		'transition':'all 0.3s'
-	});
-	setTimeout(function(){
-		$(affectedElement).css('height','');
-	})
+function deliveryAddressRadioChange() {
+	if ($('#rbAddress2').is(':checked')) { // kayıtlı teslimat adresleri seçilirse
+		selectedOption = '.deliveryAddressWrapper .divAddressOptionsWrapper'
+		nonSelectedOption = '.deliveryAddressWrapper .divNewAddressContent'
+	}
+	else if ($('#rbAddress1').is(':checked')) { // yeni teslimat adresi seçilirse
+		selectedOption = '.deliveryAddressWrapper .divNewAddressContent'
+		nonSelectedOption = '.deliveryAddressWrapper .divAddressOptionsWrapper'
+	};
 
-
-	$(triggerInput).change(function(){
-		scrollUserTo(affectedElement);
-		showNewAddressForm(triggerInput,affectedElement,classNamesToggle);
-	});
-
-
-	$(affectedElement).find('.contShopping, .saveAddingNewAddress, .saveAddingNewInvoiceAddress').click(function(e){
-		e.preventDefault();
-		removeProhibitor();
-
-		$(affectedElement).css({
-			'opacity':'0',
-			'-webkit-transform':'scale(0.95)'
-		});
-		$(affectedElement).wrapInner('<div class="innerWrapper">');
-		var classNames = (classNamesToggle).split(',');
-		setTimeout(function(){
-				$(affectedElement).addClass(classNames[1]);
-				$(affectedElement).removeClass(classNames[0]);
-				removeProhibitor();
-			var currentHeight = $(affectedElement).find('.innerWrapper').outerHeight();
-			$(affectedElement).find('.innerWrapper > *').unwrap();
-			$(affectedElement).height(currentHeight);
-			scrollUserTo(affectedElement);
-			setTimeout(function(){
-				$(affectedElement).css({
-					'opacity':'1',
-					'-webkit-transform':'scale(1)'
-				});
-				setTimeout(function(){
-					$(affectedElement).css({
-						'height':'auto'
-					});
-				},6000)
-				$(triggerInput).first().prop('checked', true);
-			},100);
-		},500);
-	})
+	switchOptions(selectedOption, nonSelectedOption);
 }
 
-function showNewAddressForm(triggerInput, affectedElement, classNamesToggle){
-		$(affectedElement).css({
-			'opacity':'0',
-			'-webkit-transform':'scale(0.95)'
-		});
-		$(affectedElement).wrapInner('<div class="innerWrapper">');
-		var classNames = (classNamesToggle).split(',');
-		scrollUserTo(affectedElement);
-		setTimeout(function(){
-			if (triggerInput != null) {
+function invoiceAddressRadioChange() {
+	if ($('#rbNewAddress1').is(':checked')) { // kayıtlı fatura adresleri seçilirse
+		selectedOption = '.divBillingAddress .divBillingAddressOptionsWrapper'
+		nonSelectedOption = '.divBillingAddress .divAddNewInvoiceAddress'
+	}
+	else if ($('#rbNewAddress2').is(':checked')) { // yeni fatura adresi seçilirse
+		selectedOption = '.divBillingAddress .divAddNewInvoiceAddress'
+		nonSelectedOption = '.divBillingAddress .divBillingAddressOptionsWrapper'
+	};
 
-				if ($(triggerInput).first().is(':checked')) {
-					$(affectedElement).addClass(classNames[1]);
-					$(affectedElement).removeClass(classNames[0]);
-					removeProhibitor();
-				}
-				else {
-					$(affectedElement).removeClass(classNames[1]);
-					$(affectedElement).addClass(classNames[0]);
-					
-					setTimeout(function(){
-						addProhibitor(affectedElement);
-					},300);
-				}
-
-			}
-			else {
-				$(affectedElement).removeClass(classNames[1]);
-				$(affectedElement).addClass(classNames[0]);
-				
-				setTimeout(function(){
-					addProhibitor(affectedElement);
-				},300);
-			}
-			var currentHeight = $(affectedElement).find('.innerWrapper').outerHeight();
-			$(affectedElement).find('.innerWrapper > *').unwrap();
-			$(affectedElement).height(currentHeight);
-			setTimeout(function(){
-				$(affectedElement).css({
-					'opacity':'1',
-					'-webkit-transform':'scale(1)'
-				});
-				setTimeout(function(){
-					$(affectedElement).css({
-						'height':'auto'
-					});
-				},6000)
-			},100);
-		},500);
+	switchOptions(selectedOption, nonSelectedOption);
 }
 
-function openClosedSections(triggerInput,affectedElement,closedHeight) {
-	$(affectedElement).css({
-		'height':closedHeight,
+function switchOptions(selectedOption, nonSelectedOption) {
+	$(nonSelectedOption).css({
 		'opacity':'0',
-		'overflow':'hidden',
 		'-webkit-transform':'scale(0.95)',
-		'transition':'all 0.3s'
+		'transition':'transform 0.3s, opacity 0.3s',
+		'-webkit-transition':'transform 0.3s, opacity 0.3s',
+		'-moz-transition':'transform 0.3s, opacity 0.3s',
+		'-ms-transition':'transform 0.3s, opacity 0.3s'
 	});
-
-	$(triggerInput).change(function(){
-		if ($(this).is(':checked')) {
-			$(affectedElement).wrapInner('<div class="innerWrapper">');
-			var closedContentHeight = $(affectedElement).find('.innerWrapper').outerHeight();
-			$(affectedElement).css({
-				'height':closedContentHeight,
-				'transition':'all 0.3s'
-			});
-			setTimeout(function(){
-				$(affectedElement).css({
-					'height':'auto',
-					'opacity':'1',
-					'-webkit-transform':'scale(1)',
-					'transition':'all 0.3s'
-				});
-				$(affectedElement).find('.innerWrapper > *').unwrap();
-				if ($(affectedElement).height() > 300) {
-					scrollUserTo(affectedElement);
-				}
-				else {
-					scrollSanitizer(affectedElement);
-				}
-			},300);
-		}
-		else {
-			$(affectedElement).css({
-				'height':$(affectedElement).outerHeight()
-			})
-			setTimeout(function(){
-				$(affectedElement).css({
-					'height':closedHeight,
-					'opacity':'0',
-					'overflow':'hidden',
-					'-webkit-transform':'scale(0.95)',
-					'transition':'all 0.3s'
-				});
-			},1)
-		}
-	})
-}
-
-function addProhibitor(unprohibitedElement) {
-	var upperProhibitorEndingPoint = $(unprohibitedElement).offset().top - $('.divWholePaymentWrapper').offset().top - 100;
-	var lowerProhibitorStartingPoint = upperProhibitorEndingPoint + $(unprohibitedElement).height();
-
-if (upperProhibitorEndingPoint < 200) {
-	$('.saveBtn').first().after('<div class="prohibitorOverlay" id="miniProhibitor" style=" position:absolute; right:0px; top:0px; z-index:999; height:40px; background-color:rgba(255,255,255,0.95); margin:33px 0 0 0; width:200px; opacity:0; ">');
-};
-	$('.divWholePaymentWrapper').prepend('<div class="prohibitorOverlay" id="upperProhibitor" style=" position:absolute; left:0px; top:-0px; z-index:999; width:100%; height:'+ (upperProhibitorEndingPoint) +'px; background-color:rgba(255,255,255,0.95); opacity:0; ">');
-	$('.divWholePaymentWrapper').append('<div class="prohibitorOverlay" id="lowerProhibitor" style=" width:100%; position:absolute; left:0px; z-index:999; opacity:0; top:'+(lowerProhibitorStartingPoint+100)+'px; height:'+ ($('.divWholePaymentWrapper').height() - lowerProhibitorStartingPoint) +'px; background-color:rgba(255,255,255,0.95); ">');
 	setTimeout(function(){
-		$('.prohibitorOverlay').each(function(){
-			$(this).css('opacity','1');
-		})
-	},100)
-}
-
-function removeProhibitor() {
-	$('.prohibitorOverlay').each(function(){
-		var dissappearingElement = $(this)
-		dissappearingElement.css('opacity','0');
+		$(nonSelectedOption).css('display','none');
+		$(selectedOption).css({
+			'display':'block',
+			'opacity':'0',
+			'-webkit-transform':'scale(0.95)',
+			'transition':'transform 0.3s, opacity 0.3s',
+			'-webkit-transition':'transform 0.3s, opacity 0.3s',
+			'-moz-transition':'transform 0.3s, opacity 0.3s',
+			'-ms-transition':'transform 0.3s, opacity 0.3s'
+		});
 		setTimeout(function(){
-			dissappearingElement.remove()
-		},400);
-	});
+			$(selectedOption).css({
+				'opacity':'1',
+				'-webkit-transform':'scale(1)'
+			});
+		},300);
+	},300);
 }
 
-function radioActions() {
-	$('.divAddressOption').find('input[type="radio"]').change(function(){
-		$('.divAddressOption').each(function(){
-			$(this).removeClass('active');	
-		})
-		$(this).parents('.divAddressOption').addClass('active');
-	});
-
-	$('.divWirePaymentOption').find('input[type="radio"]').change(function(){
-		$('.divWirePaymentOption').each(function(){
-			$(this).removeClass('active');	
-		})
-		$(this).parents('.divWirePaymentOption').addClass('active');
-	});
+function openCloseElements(openCloseSwitch, openedClosedElement) {
+	if ($(openCloseSwitch).is(':checked')) {
+		$(openedClosedElement).css({
+			'display':'block',
+			'opacity':'0',
+			'-webkit-transform':'scale(0.95)',
+			'transition':'transform 0.3s, opacity 0.3s'
+		});
+		setTimeout(function(){
+			$(openedClosedElement).css({
+				'opacity':'1',
+				'-webkit-transform':'scale(1)'
+			});
+		},300);
+	}
+	else { // yeni teslimat adresi seçilirse
+		$(openedClosedElement).css({
+			'opacity':'0',
+			'-webkit-transform':'scale(0.95)',
+			'transition':'transform 0.3s, opacity 0.3s'
+		});
+		setTimeout(function(){
+			$(openedClosedElement).css('display','none');
+		},300);
+	};
 }
+
+
 
 function editAddressInfo(affectedElement,classNamesToggle, addressId) {
 	event.preventDefault();
-	showNewAddressForm(null, affectedElement, classNamesToggle);
+	$('#rbAddress1').prop('checked',true);
+	$('#rbAddress2').prop('checked',false);
 	setTimeout(function(){
-		alert('Düzenlenecek addressId sini paslıyorum: '+addressId);
-	},2000);
+		deliveryAddressRadioChange();
+	},20)
 }
 
 function deleteAddressInfo(sender, addressId) {
@@ -319,21 +225,21 @@ function deleteAddressInfo(sender, addressId) {
 				};
 			});
 
-	//MANAGE PAGE SCROLLING
-	function scrollUserTo(scrolledElement) {
-		var scrolledElementTop = $(scrolledElement).offset().top;
-		var scrollDestination = scrolledElementTop - 225;
-		$('html,body').animate({scrollTop: scrollDestination }, '300');
-	}
-	function scrollSanitizer(sanitizedElement) {
-		var elementDestination = $(sanitizedElement).offset().top + 200;
-		var windowDestination = scrollTopVal+wH;
-		var sanitizingDestination = elementDestination-wH+100;
 
-		if (elementDestination > windowDestination) {
-			$('html,body').animate({scrollTop: sanitizingDestination }, '300');
-		};
-		//$('html,body').animate({scrollTop: scrollDestination }, '500');
-	}
-	//
 
+
+function radioActions() {
+	$('.divAddressOption').find('input[type="radio"]').change(function(){
+		$('.divAddressOption').each(function(){
+			$(this).removeClass('active');	
+		})
+		$(this).parents('.divAddressOption').addClass('active');
+	});
+
+	$('.divWirePaymentOption').find('input[type="radio"]').change(function(){
+		$('.divWirePaymentOption').each(function(){
+			$(this).removeClass('active');	
+		})
+		$(this).parents('.divWirePaymentOption').addClass('active');
+	});
+}
